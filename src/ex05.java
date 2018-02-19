760f5c5eb8672e46ad011cce05bb674720a89d47
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2018-02-19 15:55:52 +0100

    Setting up

---
+++ src/ex05.java
@@ -0,0 +1,10 @@
+
+public class ex05 {
+    public static void main(String[] args) {
+        LinkedIntList list = new LinkedIntList();
+
+    }
+    public static void countDuplicates (LinkedIntList list){
+
+    }
+}
