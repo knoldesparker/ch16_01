e4c71cd43f5237f99ea3c40b5557546823f6f832
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2018-02-24 10:21:01 +0100

    +

--- src/ex20.java
+++ src/ex20.java
@@ -1,5 +1,22 @@
 public class ex20 {
     public static void main(String[] args) {
 
+        LinkedIntList list = new LinkedIntList();
+
+        list.add(10);
+        list.add(31);
+        list.add(42);
+        list.add(23);
+        list.add(44);
+        list.add(75);
+        list.add(86);
+
+        shift(list);
+
+    }
+
+    public static void shift(LinkedIntList list) {
+        
     }
 }
+
