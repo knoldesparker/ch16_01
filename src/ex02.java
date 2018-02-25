73a42418bf33fde654c6b9b6f06f002defce6516
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2018-02-25 11:30:49 +0100

    edit ass 2

--- src/ex02.java
+++ src/ex02.java
@@ -15,19 +15,20 @@ public class ex02 {
         //Adds values
 
         //uses method with the list
-        max(list);
-        max(list2); //Idk how to display an exseption..
+        max(list);      //Output is 10
+        max(list2);     //Throws an exception NoSuchElemnt
     }
     public static void max (LinkedIntList list) {
         int max = 0; //makes a max int to save the maximum value
             for (int i = 0; i < list.size(); i++) { // runs thu the list length
                 if (list.get(i) > max) { //
                     max = list.get(i);
-                }
-                if (list.size() == 0){
-                    //throw (NoSuchElementException);
+
                 }
             }
+       if (list.size() == 0) {
+            throw new NoSuchElementException();
+        }
         System.out.println(max);
 
     }
